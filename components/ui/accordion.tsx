23ff4636dac9b73a1af5856433ef6e9e5dade1ba
tailedflox9-maker <tailedflox9@gmail.com>
2025-10-02 14:31:10 +0530

    Update accordion.tsx

--- components/ui/accordion.tsx
+++ components/ui/accordion.tsx
@@ -25,7 +25,7 @@ const AccordionTrigger = React.forwardRef<
     <AccordionPrimitive.Trigger
       ref={ref}
       className={cn(
-        'flex flex-1 items-center justify-between py-3 font-medium transition-all hover:underline [&[data-state=open]>svg]:rotate-180',
+        'flex flex-1 items-center justify-between py-3 font-medium transition-all text-sm text-zinc-300 hover:text-white [&[data-state=open]>svg]:rotate-180',
         className,
       )}
       {...props}
